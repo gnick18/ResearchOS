bf6e0d5a1ba8b099f534582f54e3bd5f530c4daa
Grant Nickles <gnickles@wisc.edu>
2026-03-06 15:31:47 -0600

    Fix Windows build: use ZIP maker instead of Squirrel

--- installer/forge.config.js
+++ installer/forge.config.js
@@ -10,16 +10,6 @@ module.exports = {
   },
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        name: 'ResearchOSInstaller',
-        authors: 'ResearchOS',
-        description: 'ResearchOS Smart Installer',
-        iconUrl: 'https://raw.githubusercontent.com/example/researchos/main/installer/assets/icon.ico',
-        setupIcon: './assets/icon.ico',
-      },
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin', 'win32'],
     },
